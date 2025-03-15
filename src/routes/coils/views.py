6ffb39cd56dadae76409a22055eb2d75578e8fcd
KonstantinPetrovichQWERTY <skatkov24@mail.ru>
2025-03-16 01:19:04 +0300

    feat: added get_all_coils

--- src/routes/coils/views.py
+++ src/routes/coils/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from typing import Optional
 import uuid
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
@@ -53,8 +55,8 @@ async def get_coil_by_id(coil_id: uuid.UUID, session: AsyncSession = Depends(get
 
 @router.patch("/api/v1/coils/{coil_id}/", response_model=CoilSchema)
 async def update_coil(
-    coil_id: uuid.UUID, 
-    coil_data: UpdatePartialCoilSchema, 
+    coil_id: uuid.UUID,
+    coil_data: UpdatePartialCoilSchema,
     session: AsyncSession = Depends(get_db),
 ):
 
@@ -71,3 +73,40 @@ async def update_coil(
     await session.refresh(coil)
 
     return coil
+
+
+@router.get("/api/v1/coils/", response_model=list[CoilSchema])
+async def get_all_coils(
+    coil_id: Optional[uuid.UUID] = None,
+    weight_gte: Optional[float] = None,
+    weight_lte: Optional[float] = None,
+    length_gte: Optional[float] = None,
+    length_lte: Optional[float] = None,
+    created_at_gte: Optional[datetime] = None,
+    created_at_lte: Optional[datetime] = None,
+    deleted_at_gte: Optional[datetime] = None,
+    deleted_at_lte: Optional[datetime] = None,
+    session: AsyncSession = Depends(get_db),
+):
+    query = select(Coil)
+
+    # It's weird, but filtering IDs by range is even weirder :)
+    # So I created `get_coil_by_id`
+    if coil_id is not None:
+        query = query.filter(Coil.coil_id == coil_id)
+
+    filters_ranges = {
+        "weight": (weight_gte, weight_lte),
+        "length": (length_gte, length_lte),
+        "created_at": (created_at_gte, created_at_lte),
+        "deleted_at": (deleted_at_gte, deleted_at_lte),
+    }
+
+    for field, (gte, lte) in filters_ranges.items():
+        if gte is not None:
+            query = query.filter(getattr(Coil, field) >= gte)
+        if lte is not None:
+            query = query.filter(getattr(Coil, field) <= lte)
+
+    result = await session.execute(query)
+    return result.scalars().all()
